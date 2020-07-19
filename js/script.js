cddf8320137e7c083dfc3dc52edcb5b154cdc677
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-19 13:33:07 -0700

    Initial commit / pseudocode

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// SunnyMoon Weather App project script
+// Variables declaration - Dates
+let myVariable
+
+// Function: jQuery / on-click event user - city
+
+function name(params) {
+    
+}
+
+
+// if / else if / else / conditionals template
+
+// Choose city input
+
+// Empty Data
+
+// Daily forecast - api
+
+// Ajax block - 1-day forecast
+
+// Ajax block - 5-day forecast
+
+if (condition) {
+    
+}
+else if (condition) {
+    
+} else {
+    
+}
+
+// LocalStorage block
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
