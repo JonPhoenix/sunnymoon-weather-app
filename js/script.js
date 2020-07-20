bda736a43b60f5dae487f4a1268e7b342fdfe0b5
Jon Phoenix <jonathan.martb@gmail.com>
2020-07-20 11:14:48 -0700

    Updating jQuery function

--- js/script.js
+++ js/script.js
@@ -7,19 +7,27 @@ let currentDate = moment().format('M/DD/YYYY');
 $(document).ready(function() {
     console.log('ready');
 
+    // Choosing city / user's input
     $('#icon-button').on('click', function(event) {
-        event.preventdefault();
+        event.preventDefault();
         let cityInput = $('#input').val(); // Inputs and saves a city
         let inputCities = []; // Empty array to contain input cities
+
+        inputCities = JSON.parse(localStorage.getItem('inputCities')) || []; // Getting input cities
+        inputCities.push(cityInput); // Pushing input cities to the empty array
+        localStorage.setItem('inputCities', JSON.stringify(inputCities)); // Saving input cities to localStorage
+
+        displayWeather(cityInput); // Calling in function
     })
 
-    // if / else if / else / conditionals template
+    // Daily forecast function & API
+    function displayWeather(cityInput) {
 
-    // Choose city input
+    }
 
     // Empty Data
 
-    // Daily forecast - api
+    // if / else if / else / conditionals template
 
     // Ajax block - 1-day forecast
 
